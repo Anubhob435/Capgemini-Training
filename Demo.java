2de10762d3b18f89b26a0692d854f8c632662f3b
Anubhob <anubhob493@gmail.com>
2026-01-07 21:42:44 +0530

    create repo

---
+++ Demo.java
@@ -0,0 +1,6 @@
+public class Demo extends Test{
+    public static void main(String[] args) {
+        int result = addition(5, 11);
+        System.out.println("The sum is: " + result);
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+## Date 07.01.2026
+### Day 0 
+- Syllabus and training structure
+- test patterns
+- some intro into java
+- eclipse for java enterprise
+- biometric registration
